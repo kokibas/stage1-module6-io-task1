9f950c822c8eb65fca102ea8f5a2686e89100a4f
Nurym <nurym0807@gmail.com>
2023-04-28 09:53:36 +0600

    check01

--- src/main/java/com/epam/mjc/io/FileReader.java
+++ src/main/java/com/epam/mjc/io/FileReader.java
@@ -1,11 +1,50 @@
 package com.epam.mjc.io;
 
+import java.io.BufferedReader;
 import java.io.File;
-
+import java.io.IOException;
 
 public class FileReader {
+    public static Profile getDataFromFile(File file) throws IOException {
+        java.io.FileReader fileReader = new java.io.FileReader(file);
+        BufferedReader bufferedReader = new BufferedReader(fileReader);
 
-    public Profile getDataFromFile(File file) {
-        return new Profile();
+        StringBuilder stringBuilder = new StringBuilder();
+        String line;
+        while ((line = bufferedReader.readLine()) != null) {
+            stringBuilder.append(line);
+            stringBuilder.append(System.lineSeparator());
+        }
+        String fileContent = stringBuilder.toString();
+
+        String[] lines = fileContent.split(System.lineSeparator());
+        String name = null;
+        int age = 0;
+        String email = null;
+        Long phone = null;
+        for (String l : lines) {
+            String[] parts = l.split(":");
+            if (parts.length == 2) {
+                String key = parts[0].trim();
+                String value = parts[1].trim();
+                switch (key) {
+                    case "Name":
+                        name = value;
+                        break;
+                    case "Age":
+                        age = Integer.parseInt(value);
+                        break;
+                    case "Email":
+                        email = value;
+                        break;
+                    case "Phone":
+                        phone = Long.parseLong(value);
+                        break;
+                }
+            }
+        }
+        bufferedReader.close();
+
+        return new Profile(name, age, email, phone);
     }
 }
